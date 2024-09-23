46ddc9504976952761d90f733f918932e89cde3b
Khue Nguyen <Z5483Y@gmail.com>
2024-09-23 14:15:34 -0400

    fix: add proper tab change mechanism

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import Home from './Home';
+import React, { useState } from 'react';
+import Home from './Home.js';
+import AboutMe from './AboutMe.js';
+
+import Header from './Header.js';
+import Footer from './Footer.js';
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    const handleTabChange = (newValue) => {
+        setSelectedTab(newValue);
+    };
+
+    return (
+        <div className="App">
+            <Header onChangeTab={handleTabChange} />
+            {selectedTab === 0 && <Home onChangeTab={handleTabChange} />}
+            {selectedTab === 1 && <AboutMe onChangeTab={handleTabChange} />}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
-function Header() {
-  return (
-    <header>
-      <h1 className="header-logo">
-        <a href="/" className="header-menu-item">khuen</a>
-      </h1>
-      <nav>
-        <ul className="header-menu">
-          <li><a href="/" className="header-menu-item" id="header-item-home">Home</a></li>
-          <li><a href="/blog/" className="header-menu-item" id="header-item-blog">Blog</a></li>
-          <li><a href="/about-me.html" className="header-menu-item" id="header-item-about-me">About Me</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+function Header({ onChangeTab }) {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+        onChangeTab(newValue);
+    };
+
+    return (
+        <header>
+            <h1 className="header-logo">
+                <a href="/" className="header-menu-item">khuen</a>
+            </h1>
+            <nav>
+                <ul className="header-menu">
+                    <li>
+                        <a
+                            href="/"
+                            className="header-menu-item"
+                            id="header-item-home"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                handleChange(0);
+                            }}
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/blog/"
+                            className="header-menu-item"
+                            id="header-item-blog"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                handleChange(1);
+                            }}
+                        >
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/about-me.html"
+                            className="header-menu-item"
+                            id="header-item-about-me"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                handleChange(2);
+                            }}
+                        >
+                            About Me
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
-
