4ed54512fd2665aee2d1010efcd44b3ed8525b85
Khue Nguyen <Z5483Y@gmail.com>
2024-10-14 10:56:46 -0400

    feat: add mobile header

--- src/Header.js
+++ src/Header.js
@@ -1,63 +1,158 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 
+import Box from "@mui/material/Box";
+import { MdMenu } from "react-icons/md";
+
 function Header({ onChangeTab, currentTab }) {
-  const handleChange = (newValue) => {
-    onChangeTab(newValue);
+  const [isMobile, setIsMobile] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
   };
 
+  const handleChange = (newValue) => {
+    onChangeTab(newValue);
+    toggleMenu();
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <header>
-      <h1 className="header-logo">
-        <a href="/" className="header-menu-item">
-          khuen
-        </a>
-      </h1>
-      <nav>
-        <ul className="header-menu">
-          <li>
-            <a
-              href="/"
-              className="header-menu-item"
-              id="header-item-home"
-              onClick={(e) => {
-                e.preventDefault();
-                handleChange(0);
+    <>
+      {isMobile ? (
+        <header>
+          <h1 className="header-logo">
+            <MdMenu onClick={toggleMenu} />
+          </h1>
+          {showMenu && (
+            <Box
+              sx={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100dvw",
+                height: "95dvh",
+                marginTop: "5dvh",
+                bgcolor: "rgba(0, 0, 0, 0.01)",
+                backdropFilter: "blur(50px)",
+                zIndex: 1000,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              Home
+          <nav>
+            <ul className="header-menu">
+              <li>
+                <a
+                  href="/"
+                  className="header-menu-item"
+                  id="header-item-home"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleChange(0);
+                  }}
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/blog"
+                  className="header-menu-item"
+                  id="header-item-blog"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleChange(1);
+                  }}
+                >
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/about-me"
+                  className="header-menu-item"
+                  id="header-item-about-me"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleChange(2);
+                  }}
+                >
+                  About Me
+                </a>
+              </li>
+            </ul>
+          </nav>
+            </Box>
+          )}
+        </header>
+      ) : (
+        <header>
+          <h1 className="header-logo">
+            <a href="/" className="header-menu-item">
+              khuen
             </a>
-          </li>
-          <li>
-            <a
-              href="/blog"
-              className="header-menu-item"
-              id="header-item-blog"
-              onClick={(e) => {
-                e.preventDefault();
-                handleChange(1);
-              }}
-            >
-              Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="/about-me"
-              className="header-menu-item"
-              id="header-item-about-me"
-              onClick={(e) => {
-                e.preventDefault();
-                handleChange(2);
-              }}
-            >
-              About Me
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </h1>
+          <nav>
+            <ul className="header-menu">
+              <li>
+                <a
+                  href="/"
+                  className="header-menu-item"
+                  id="header-item-home"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleChange(0);
+                  }}
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/blog"
+                  className="header-menu-item"
+                  id="header-item-blog"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleChange(1);
+                  }}
+                >
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/about-me"
+                  className="header-menu-item"
+                  id="header-item-about-me"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleChange(2);
+                  }}
+                >
+                  About Me
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      )}
+    </>
   );
 }
 
