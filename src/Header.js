5a2fcd37b7118f9be3edde936f8a3d6a60b8e607
Khue Nguyen <Z5483Y@gmail.com>
2024-10-23 10:55:17 -0400

    fix(mobile): hide menu after one of the item is clicked

--- src/Header.js
+++ src/Header.js
@@ -69,13 +69,13 @@ function Header() {
               <nav>
                 <ul className="header-menu">
                   <li>
-                    <Link to="/" className="header-menu-item" id="header-item-home">Home</Link>
+                    <Link to="/" className="header-menu-item" id="header-item-home" onClick={toggleMenu}>Home</Link>
                   </li>
                   <li>
-                    <Link to="/blog" className="header-menu-item" id="header-item-blog">Blog</Link>
+                    <Link to="/blog" className="header-menu-item" id="header-item-blog" onClick={toggleMenu}>Blog</Link>
                   </li>
                   <li>
-                    <Link to="/about-me" className="header-menu-item" id="header-item-about-me">About Me</Link>
+                    <Link to="/about-me" className="header-menu-item" id="header-item-about-me" onClick={toggleMenu}>About Me</Link>
                   </li>
                 </ul>
               </nav>
